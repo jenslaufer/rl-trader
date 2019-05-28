6c96b813d696094d5e2e4f513be532c82497d30e
Jens Laufer <jenslaufer@gmail.com>
2019-05-28 09:40:07 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages, find_namespace_packages
+
+setup(name='rltrader',
+      version='0.1.0',
+      author='Jens Laufer',
+      author_email='jenslaufer@gmail.com',
+      install_requires=['stable-baselines==2.5.1',
+                        'gym==0.12.1',
+                        'pandas==0.24.2',
+                        'scikit-learn==0.21.1',
+                        'config==0.4.2',
+                        'keras==2.2.4',
+                        'keras-rl==0.4.2'],
+      packages=find_namespace_packages(where='src'),
+      package_dir={'': 'src'}
+      )
