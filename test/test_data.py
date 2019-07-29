5a9044fe40cc351f894d4dc8d720bfc584a4f0bb
Jens Laufer <jenslaufer@gmail.com>
2019-07-29 14:11:58 +0200

    fixed test

--- test/test_data.py
+++ test/test_data.py
@@ -5,6 +5,6 @@ import os
 
 def test_read_csv_data_frame():
     data = rldata.CsvFileDataFrameData(os.path.dirname(
-        os.path.realpath(__file__))+"/../src/data/btc.csv")
+        os.path.realpath(__file__))+"/data/test.csv")
 
-    assert len(data.frame) == 2099760
+    assert len(data.frame) == 2
