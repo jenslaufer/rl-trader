713ef8a7ec886dd9dd7e91b45f22d0bc64c9ed3a
Jens Laufer <jenslaufer@gmail.com>
2019-08-01 10:50:31 +0200

    added reward

--- src/rltrader/rewards.py
+++ src/rltrader/rewards.py
@@ -48,3 +48,24 @@ def net_value_reward_wrong_action_penalty(old, current, action, obs, done):
             reward = reward-1000000
 
     return reward
+
+
+def net_value_reward_wrong_action_slight_penalty(old, current, action, obs, done):
+    old_balance = old['balance']
+    old_asset_balance = old['asset_balance']
+    old_price = old['price']
+
+    balance = current['balance']
+    asset_balance = current['asset_balance']
+    price = current['price']
+
+    reward = net_value_reward(old, current, action, obs, done)
+
+    if action == 1:
+        if balance == old_balance:
+            reward = reward-1000
+    elif action == 2:
+        if asset_balance == old_asset_balance:
+            reward = reward-1000
+
+    return reward
