ba7b19c3f82e809b21150b243758474607b78fe9
tonyskulk <tony.skulk@gmail.com>
2019-08-16 12:03:39 +0200

    changed location reference again

--- src/rltrader/trainer.py
+++ src/rltrader/trainer.py
@@ -93,6 +93,7 @@ if __name__ == '__main__':
     print('current working directory: ' + os.getcwd())
     print('module location: ' + __file__)
     logging.config.dictConfig(
-        yaml.load(open('/usr/local/lib/python3.5/dist-packages/rltrader/logging.yml', 'r'), Loader=yaml.FullLoader))
+        yaml.load(open('logging.yml', 'r'), Loader=yaml.FullLoader))
+        # yaml.load(open('/usr/local/lib/python3.5/dist-packages/rltrader/logging.yml', 'r'), Loader=yaml.FullLoader))
     logging.info('Logging module setup finished.')
     do_train()
